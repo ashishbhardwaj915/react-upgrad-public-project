67aa37b2cc49bce1b2ed4ee3c5e3fbe69c04507d
ashishbhardwaj <ashishbhardwaj915@gmail.com>
2023-05-28 23:50:27 +0530

    FunctionalityIssueInUpgradBackendCode coudntComple

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -60,10 +60,23 @@ export const Signup = () => {
                 <div>
                     <div className="row">
                         <div className="mx-auto col-10 col-md-8 col-lg-4">
-                            <div className="text-center" style={{ margin: "20px" }}>
-                                <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="logo" width="30" height="24" />
-                            </div>
 
+                            <div className="text-center" style={{ marginTop: "100px" }}>
+                                {/* <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="logo" width="30" height="24" /> */}
+                                <button type="button" id="circular_button" class="btn btn-success btn-circle">
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"></path>
+                                    </svg>
+
+                                </button>
+                                <div className="text-center">
+
+                                    <p style={{ color: "black", fontSize: "20px" }}>Signup</p>
+                                </div>
+
+
+                            </div>
                             <form className="form-floating" onSubmit={onSignUpSubmitHandler}>
                                 <div className="form-floating mb-3">
                                     <input type="name" className="form-control" id="floatingInput" placeholder="name" onChange={(e) => setFirstName(e.target.value)} />
